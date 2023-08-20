d9b1c73e06bfdabfe15c8930cfba013194df1daa
niruthikka24 <niruthikka.19@cse.mrt.ac.lk>
2023-08-20 13:22:37 +0530

    user, vehicle, passenger tables done, er diagram updated

--- db/database.js
+++ db/database.js
@@ -53,6 +53,57 @@ const insertBooking = (user_id, trip_id, is_paid,traveller_count, status, total_
     });
 };
 
+const insertVehicle = (vehicle_name, vehicle_image, row_count, column_count) => {
+  db.transaction(tx => {
+      tx.executeSql(
+        'INSERT INTO vehicle (vehicle_name, vehicle_image, row_count, column_count) VALUES (?,?,?,?)',
+        [vehicle_name, vehicle_image, row_count, column_count],
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) {
+            console.log("Vehicle record inserted with ID:", {insertId});
+          }
+        },
+        (_, error) => {
+          console.log("Error inserting vehicle record:", error);
+        }
+      );
+    });
+};
+
+const insertUser = (first_name, last_name, address, contact_number) => {
+  db.transaction(tx => {
+      tx.executeSql(
+        'INSERT INTO user (first_name, last_name, address, contact_number) VALUES (?,?,?,?)',
+        [first_name, last_name, address, contact_number],
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) {
+            console.log("User record inserted with ID:", {insertId});
+          }
+        },
+        (_, error) => {
+          console.log("Error inserting user record:", error);
+        }
+      );
+    });
+};
+
+const insertPassenger = (passport_number, booking_id, name) => {
+  db.transaction(tx => {
+      tx.executeSql(
+        'INSERT INTO passenger (passport_number, booking_id, name) VALUES (?,?,?)',
+        [passport_number, booking_id, name],
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) {
+            console.log("Passenger record inserted with ID:", {insertId});
+          }
+        },
+        (_, error) => {
+          console.log("Error inserting passenger record:", error);
+        }
+      );
+    });
+};
+
 const dropDatabaseTables =  () => {
     try {
         db.transaction((tx) => {
@@ -80,6 +131,46 @@ const dropDatabaseTables =  () => {
               }
             );
           });
+
+          db.transaction((tx) => {
+            tx.executeSql(
+              "Drop TABLE vehicle",
+              [],
+              () => {
+                console.log("Vehicle Table dropped successfully.");
+              },
+              (error) => {
+                console.log("Error dropping vehicle table:", error);
+              }
+            );
+          });
+
+          db.transaction((tx) => {
+            tx.executeSql(
+              "Drop TABLE user",
+              [],
+              () => {
+                console.log("User Table dropped successfully.");
+              },
+              (error) => {
+                console.log("Error dropping user table:", error);
+              }
+            );
+          });
+
+          db.transaction((tx) => {
+            tx.executeSql(
+              "Drop TABLE passenger",
+              [],
+              () => {
+                console.log("Passenger Table dropped successfully.");
+              },
+              (error) => {
+                console.log("Error dropping passenger table:", error);
+              }
+            );
+          });
+
       } catch (error) {
         console.log("Error executing SQL statement:", error);
       }
@@ -123,6 +214,64 @@ const setupDatabase = () => {
         }
       );
     });
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS "vehicle" (
+          "vehicle_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+          "vehicle_name" TEXT,
+          "vehicle_image" TEXT,
+          "row_count"	INTEGER,
+          "column_count"	INTEGER
+        );`,
+        [],
+        () => {
+          console.log("Vehicle Table created successfully.");
+        },
+        (error) => {
+          console.log("Error creating vehicle table:", error);
+        }
+      );
+    });
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS "user" (
+          "user_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+          "first_name"	TEXT,
+          "last_name"	TEXT,
+          "address"	TEXT,
+          "contact_number"	TEXT
+        );`,
+        [],
+        () => {
+          console.log("User Table created successfully.");
+        },
+        (error) => {
+          console.log("Error creating user table:", error);
+        }
+      );
+    });
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS "passenger" (
+          "passport_number"	TEXT NOT NULL,
+          "booking_id"	INTEGER,
+          "name"	TEXT,
+          FOREIGN KEY("booking_id") REFERENCES "booking"("booking_id"),
+          PRIMARY KEY("passport_number","booking_id")
+        );`,
+        [],
+        () => {
+          console.log("Passenger Table created successfully.");
+        },
+        (error) => {
+          console.log("Error creating passenger table:", error);
+        }
+      );
+    });
+
   } catch (error) {
     console.log("Error executing SQL statement:", error);
   }
@@ -133,7 +282,15 @@ const setupDatabase = () => {
         insertPlanet("Jupiter")
         insertPlanet("mars")
         insertBooking(1, 1, 1, 3, 'Upcoming', 2500.0, '1A,1B,1C')
-
+        insertVehicle('NASA', 'nasa.png', 6, 5)
+        insertVehicle('SpaceX', 'spacex.jpeg', 7, 4)
+        insertVehicle('Blue Origin', 'blue_origin.jpg', 8, 4)
+        insertVehicle('Galactic', 'galactic_spaceship.jpg', 5, 6)
+        insertVehicle('Mars Rover', 'mars_rover.jpg', 6, 6)
+        insertUser('Ravi', 'Silva', '123 Galle Rd, Colombo', '+94 77 123 4567')
+        insertPassenger('P12345', 1, 'Laksika')
+        insertPassenger('P67890', 1, 'Niruthikka')
+        insertPassenger('P24681', 1, 'Nishaanthini')
       }
     })
     .catch((error) => {
@@ -168,6 +325,9 @@ export const database = {
   getPlanets,
   insertPlanet,
   insertBooking,
+  insertUser,
+  insertVehicle,
+  insertPassenger,
   setupDatabase,
   dropDatabaseTables,
   tableExists
